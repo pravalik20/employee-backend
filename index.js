005428d303eabba5f8e043d75bf359f92126285d
PRAVALIK K. <PRAVALIKK@slpl.com>
2026-03-11 14:25:01 +0530

    port update

--- index.js
+++ index.js
@@ -194,6 +194,6 @@ app.delete("/cart", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, '0.0.0.0', () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
